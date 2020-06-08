2c8879117d6706e1cec6f996f12aabd8e13d2305
越岑 <anettp793607@gmail.com>
2020-06-09 01:20:56 +0800

    个人信息ui修改

--- app/src/main/java/com/android/cai_lai_la/fragment/NavMeFragment.java
+++ app/src/main/java/com/android/cai_lai_la/fragment/NavMeFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -45,15 +46,75 @@ public class NavMeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Button touxiang = (Button)view.findViewById(R.id.personalInfo_button_first);
         Drawable drawable1 = getResources().getDrawable(R.drawable.headsculp);
-        drawable1.setBounds(0, 0, 80, 80);
-        Drawable drawable2 = getResources().getDrawable(R.drawable.jiantou);
-        drawable2.setBounds(0, 0, 80, 80);
-        touxiang.setCompoundDrawables(drawable1, null, drawable2, null);
+        drawable1.setBounds(0, 0, 120, 120);
+        touxiang.setCompoundDrawables(null, drawable1, null, null);
         view.findViewById(R.id.personalInfo_button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             }
         });
+        Button wallet = (Button)view.findViewById(R.id.personalInfo_button_second);
+        Drawable drawable2 = getResources().getDrawable(R.drawable.wallet);
+        drawable2.setBounds(0, 0, 60, 60);
+        wallet.setCompoundDrawables(null, drawable2, null, null);
+        view.findViewById(R.id.personalInfo_button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        Button shipped = (Button)view.findViewById(R.id.personalInfo_button_third);
+        Drawable drawable3 = getResources().getDrawable(R.drawable.shipped);
+        drawable3.setBounds(0, 0, 60, 60);
+        shipped.setCompoundDrawables(null, drawable3, null, null);
+        view.findViewById(R.id.personalInfo_button_third).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        Button comment = (Button)view.findViewById(R.id.personalInfo_button_fourth);
+        Drawable drawable4 = getResources().getDrawable(R.drawable.comment);
+        drawable4.setBounds(0, 0, 60, 60);
+        comment.setCompoundDrawables(null, drawable4, null, null);
+        view.findViewById(R.id.personalInfo_button_fourth).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+        });
+        Button aftersale_service = (Button)view.findViewById(R.id.personalInfo_button_fifth);
+        Drawable drawable5 = getResources().getDrawable(R.drawable.afs);
+        drawable5.setBounds(0, 0, 60, 60);
+        aftersale_service.setCompoundDrawables(null, drawable5, null, null);
+        view.findViewById(R.id.personalInfo_button_fourth).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        Button order = (Button)view.findViewById(R.id.personalInfo_button_sixth);
+        view.findViewById(R.id.personalInfo_button_sixth).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        Button address = (Button)view.findViewById(R.id.personalInfo_button_seventh);
+        view.findViewById(R.id.personalInfo_button_seventh).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        TextView jiantou1 = (TextView) view.findViewById(R.id.personalInfo_textView_first);
+        Drawable drawable6 = getResources().getDrawable(R.drawable.jiantou);
+        drawable6.setBounds(0, 0, 40, 40);
+        jiantou1.setCompoundDrawables(null, null, drawable6, null);
+
+        TextView jiantou2 = (TextView) view.findViewById(R.id.personalInfo_textView_second);
+        Drawable drawable7 = getResources().getDrawable(R.drawable.jiantou);
+        drawable7.setBounds(0, 0, 40, 40);
+        jiantou2.setCompoundDrawables(null, null, drawable7, null);
     }
 
 
