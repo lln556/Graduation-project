9252a185849d9e0f799985fd338384890d706d67
越岑 <anettp793607@gmail.com>
2020-06-08 19:54:04 +0800

    正在设计个人信息界面

--- app/src/main/java/com/android/cai_lai_la/fragment/NavMeFragment.java
+++ app/src/main/java/com/android/cai_lai_la/fragment/NavMeFragment.java
@@ -7,11 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.android.cai_lai_la.R;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+
+import com.android.cai_lai_la.R;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link NavMeFragment#newInstance} factory method to
