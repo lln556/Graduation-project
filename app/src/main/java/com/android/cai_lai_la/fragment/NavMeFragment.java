9c6cf6c5faf2d066ee1c8505c093097f754a54ce
越岑 <anettp793607@gmail.com>
2020-06-08 19:37:33 +0800

    正在设计个人信息界面

--- app/src/main/java/com/android/cai_lai_la/fragment/NavMeFragment.java
+++ app/src/main/java/com/android/cai_lai_la/fragment/NavMeFragment.java
@@ -1,14 +1,17 @@
 package com.android.cai_lai_la.fragment;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.android.cai_lai_la.R;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
+import androidx.navigation.fragment.NavHostFragment;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link NavMeFragment#newInstance} factory method to
@@ -36,5 +39,23 @@ public class NavMeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_nav_me, container, false);
+
+
     }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button touxiang = (Button)view.findViewById(R.id.personalInfo_button_first);
+        Drawable drawable1 = getResources().getDrawable(R.drawable.headsculp);
+        drawable1.setBounds(0, 0, 80, 80);
+        Drawable drawable2 = getResources().getDrawable(R.drawable.jiantou);
+        drawable2.setBounds(0, 0, 80, 80);
+        touxiang.setCompoundDrawables(drawable1, null, drawable2, null);
+        view.findViewById(R.id.personalInfo_button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+
+
 }
